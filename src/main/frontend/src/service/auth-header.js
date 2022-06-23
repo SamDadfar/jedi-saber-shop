49eb08747c57d066c7742d7ee4efd6539f057a11
SamDadfar <samdadfar@yahoo.com>
2022-06-23 23:22:40 +0200

    added bootstrap

--- src/main/frontend/src/service/auth-header.js
+++
@@ -1,8 +0,0 @@
-export default function authHeader() {
-  const data = JSON.parse(localStorage.getItem("data"));
-
-  if (data && data.token) {
-    return { Authorization: `Bearer ${data.token}` };
-  }
-  return {};
-}
